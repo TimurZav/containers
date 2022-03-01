beddb87277247012f3f4088e691d2ade1097aa76
Timur <timurka.zavyalov@mail.ru>
2022-03-01 11:33:30 +0300

    init_eight

--- scripts/morservice.py
+++ scripts/morservice.py
@@ -1,11 +1,9 @@
 import csv
-import datetime
 import os
 import logging
-import re
 import sys
 import json
-from itertools import cycle, tee
+from itertools import tee
 
 
 month_list = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь",
@@ -33,51 +31,44 @@ def pairwise(iterable):
     return zip(a, b)
 
 
-class OoclCsv(object):
+def process(input_file_path):
+    context = dict()
+    parsed_data = list()
+    with open(input_file_path, newline='') as csvfile:
+        lines = list(csv.reader(csvfile))
 
-    def __init__(self):
-        pass
+    logging.info(u'lines type is {} and contain {} items'.format(type(lines), len(lines)))
+    logging.info(u'First 3 items are: {}'.format(lines[:3]))
 
-    def process(self, input_file_path):
-        context = dict()
-        parsed_data = list()
-        with open(input_file_path, newline='') as csvfile:
-            lines = list(csv.reader(csvfile))
+    for ir, line in enumerate(lines):
+        logging.info(u'line {} is {}'.format(ir, line))
+        if ir == 0:
+            text = line[0].split()
+            for month in text:
+                if month.isdigit():
+                    year = int(month)
+                if month in month_list:
+                    month_digit = month_list.index(month) + 1
+                    # year = text.index(month) + 1
+            context["month"] = month_digit
+            context["year"] = year
+            logging.info(u"context now is {}".format(context))
+            continue
+        if ir > 0 and line[0] == 'АО "НЛЭ"':
+            for value, next_value in pairwise(lines[ir+2:ir+6]):
+                # print(value)
+                parsed_record = dict()
+                parsed_record["direction"] = 'import'
+                parsed_record["is_empty"] = True if value[1] == 'порожние' else False
+                parsed_record["container_type"] = 'REF' if value[1] == 'из них реф.' else None
+                parsed_record["teu"] = float(value[9]) - float(next_value[9]) if value[1] == 'груженые' else \
+                    float(value[9])
+                record = merge_two_dicts(context, parsed_record)
+                logging.info(u"record is {}".format(record))
+                parsed_data.append(record)
 
-        logging.info(u'lines type is {} and contain {} items'.format(type(lines), len(lines)))
-        logging.info(u'First 3 items are: {}'.format(lines[:3]))
-
-        for ir, line in enumerate(lines):
-            logging.info(u'line {} is {}'.format(ir, line))
-            if ir == 0:
-                # month = line[0].rsplit(' ', 10)
-                # print(month)
-                text = line[0].split()
-                for month in text:
-                    if month.isdigit():
-                        year = int(month)
-                    if month in month_list:
-                        month_digit = month_list.index(month) + 1
-                        # year = text.index(month) + 1
-                context["month"] = month_digit
-                context["year"] = year
-                logging.info(u"context now is {}".format(context))
-                continue
-            if ir > 0 and line[0] == 'АО "НЛЭ"':
-                for value, next_value in pairwise(lines[ir+2:ir+6]):
-                    # print(value)
-                    parsed_record = dict()
-                    parsed_record["direction"] = 'import'
-                    parsed_record["is_empty"] = True if value[1] == 'порожние' else False
-                    parsed_record["container_type"] = 'REF' if value[1] == 'из них реф.' else None
-                    parsed_record["teu"] = float(value[9]) - float(next_value[9]) if value[1] == 'груженые' else \
-                        float(value[9])
-                    record = merge_two_dicts(context, parsed_record)
-                    logging.info(u"record is {}".format(record))
-                    parsed_data.append(record)
-
-        logging.error(u"About to write parsed_data to output: {}".format(parsed_data))
-        return parsed_data
+    logging.error(u"About to write parsed_data to output: {}".format(parsed_data))
+    return parsed_data
 
 
 # input_file_path = "/home/timur/PycharmWork/containers/morservice/csv/Морсервис_Контейнеры тн и ДФЭ 12.21.xls.csv"
@@ -89,7 +80,7 @@ output_file_path = os.path.join(output_folder, basename+'.json')
 print("output_file_path is {}".format(output_file_path))
 
 
-parsed_data = OoclCsv().process(input_file_path)
+parsed_data = process(input_file_path)
 
 with open(output_file_path, 'w', encoding='utf-8') as f:
     json.dump(parsed_data, f, ensure_ascii=False, indent=4)
